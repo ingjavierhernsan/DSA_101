738b757ea3719bf467c66d763e2cbac424ece9e3
jhs <ingjavierhernsan@hotmail.com>
2025-03-15 18:33:38 -0700

    Bubble sort

---
+++ Python/bubble_sort.py
@@ -0,0 +1,18 @@
+my_array = [7, 3, 9, 12, 11]
+
+print("Array: ", my_array)
+
+n = len(my_array)
+for i in range(n-1):
+	swapped = False
+	for j in range(n-i-1):
+		if my_array[j] > my_array[j+1]:	
+			temp_num = my_array[j]
+			my_array[j] = my_array[j+1]
+			my_array[j+1] = temp_num
+            #my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+			swapped = True
+		if not swapped:
+			break
+
+print("Sorted array: ", my_array)
